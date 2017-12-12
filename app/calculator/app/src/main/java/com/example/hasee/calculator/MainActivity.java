b790ff1677e6bff0091387407f3d4bca1a93be82
zz51233273 <1033527843@qq.com>
2017-12-12 23:22:01 +0800

    test

--- app/calculator/app/src/main/java/com/example/hasee/calculator/MainActivity.java
+++ app/calculator/app/src/main/java/com/example/hasee/calculator/MainActivity.java
@@ -142,8 +142,6 @@ public class MainActivity extends AppCompatActivity {
                         }
                         break;
                     }
-                    default:
-                        break;
                 }
             } catch (Exception e) {
             }
