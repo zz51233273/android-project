02c4b973d9fad69aaaf7d8fda04ea8b2ba11bc86
zz51233273 <1033527843@qq.com>
2018-01-02 19:02:51 +0800

    test

---
+++ app/MyTabActivity/app/src/main/java/com/example/hasee/mytabactivity/IntentTabActivity.java
@@ -0,0 +1,13 @@
+package com.example.hasee.mytabactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IntentTabActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tabs1);
+    }
+}
