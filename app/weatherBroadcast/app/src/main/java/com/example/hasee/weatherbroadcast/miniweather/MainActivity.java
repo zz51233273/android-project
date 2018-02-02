444a27cdb74f09431ad2c4003eea9c5a50f3a528
zz51233273 <1033527843@qq.com>
2018-02-02 19:12:50 +0800

    test

--- app/weatherBroadcast/app/src/main/java/com/example/hasee/weatherbroadcast/miniweather/MainActivity.java
+++ app/weatherBroadcast/app/src/main/java/com/example/hasee/weatherbroadcast/miniweather/MainActivity.java
@@ -1,25 +1,27 @@
 package com.example.hasee.weatherbroadcast.miniweather;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.hasee.weatherbroadcast.util.LocationUtils;
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+
 import com.example.hasee.weatherbroadcast.R;
 import com.example.hasee.weatherbroadcast.bean.TodayWeather;
 
@@ -34,6 +36,8 @@ import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.example.hasee.weatherbroadcast.database.DBManager;
 import com.example.hasee.weatherbroadcast.util.NetUtil;
@@ -43,15 +47,19 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     private static final int UPDATE_TODAY_WEATHER = 1;
 
+    public BDLocationListener BaiDuListener = new MylocationListener();
+    public LocationClient mLocationClient = null;
+    private boolean isFirstLocate = true;
+    private LocationClientOption option = new LocationClientOption();
+
     private ImageView mUpdateBtn;
-    private ImageView gps_btn;
+    private ImageView lbs_btn;
     private ImageView mCitySelect;
     private TextView cityTv, timeTv, humidityTv, weekTv, pmDataTv, pmQualityTv,
             temperatureTv, climateTv, windTv, city_name_Tv;
     private ImageView weatherImg, pmImg;
 
     private String code="";
-    private  boolean flag=false;
     private Handler mHandler = new Handler() {
         public void handleMessage(android.os.Message msg) {
             switch (msg.what) {
@@ -70,6 +78,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
+        mLocationClient = new LocationClient(getApplicationContext());//声明LocationClient类
+        mLocationClient.registerLocationListener(BaiDuListener);    //注册监听函数
         new DBManager(getApplicationContext()).writeData();
         //判断网络状态
         if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
@@ -80,27 +90,14 @@ public class MainActivity extends Activity implements View.OnClickListener {
             Toast.makeText(MainActivity.this, "网络挂了！", Toast.LENGTH_LONG).show();
         }
         initView();
+        JudgePermission();
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        initPermission();//针对6.0以上版本做权限适配
-        if (flag) {
-            getNetworkLocation();
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        LocationUtils.unRegisterListener(this);
-    }
     void initView(){        //初始化对象
         mUpdateBtn = (ImageView) findViewById(R.id.title_update_btn);
         mUpdateBtn.setOnClickListener(this);
-        gps_btn=(ImageView)findViewById(R.id.title_location);
-        gps_btn.setOnClickListener(this);
+        lbs_btn=(ImageView)findViewById(R.id.title_location);
+        lbs_btn.setOnClickListener(this);
         mCitySelect = (ImageView) findViewById(R.id.title_city_manager);
         mCitySelect.setOnClickListener(this);
         city_name_Tv = (TextView) findViewById(R.id.title_city_name);
@@ -141,11 +138,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
             updateWeatherData();
         }
         else if(view.getId()==R.id.title_location){
-            if (flag) {
-                getNetworkLocation();
-            } else {
-                Toast.makeText(this, "no permission", Toast.LENGTH_SHORT).show();
-            }
+            onRefresh();
         }
     }
 
@@ -317,66 +310,118 @@ public class MainActivity extends Activity implements View.OnClickListener {
         }
     }
 
-    private void initPermission(){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            //检查权限
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                    || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                //请求权限
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-            } else {
-                flag = true;
+    public class MylocationListener implements BDLocationListener{
+        @Override
+        public  void onReceiveLocation(BDLocation location){
+            final StringBuilder currentPosition = new StringBuilder();
+            Log.d("test",location.getLocType()+"");
+            if(location.getLocType() == BDLocation.TypeGpsLocation){   // GPS定位结果
+                currentPosition.append("\nGPS定位成功");
+                currentPosition.append("\n");
+            }else if (location.getLocType() == BDLocation.TypeNetWorkLocation){  // Net定位结果
+                Log.d("map test","net");
+                currentPosition.append("[地址]");
+                currentPosition.append(location.getAddrStr());    //获取地址信息
+                currentPosition.append("\n[运营商]");
+                currentPosition.append(location.getOperators());    //获取运营商信息
+                currentPosition.append("\n网络定位成功！");
+                currentPosition.append("\n");
+            }else if (location.getLocType() == BDLocation.TypeOffLineLocation) {                // 离线定位结果
+                Log.d("map test","Offline");
+                currentPosition.append("\ndescribe : ");
+                currentPosition.append("离线定位成功，离线定位结果也是有效的");
+            } else if (location.getLocType() == BDLocation.TypeServerError) {
+                currentPosition.append("\ndescribe : ");
+                currentPosition.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因\n");
+            } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+                currentPosition.append("\ndescribe : ");
+                currentPosition.append("网络不同导致定位失败，请检查网络是否通畅\n");
+            } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+                currentPosition.append("\ndescribe : ");
+                currentPosition.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机\n");
+            }else {
+                currentPosition.append("定位失败。错误码：\n");
+                currentPosition.append(location.getLocType()).append("\n");
             }
-        } else {
-            flag = true;
+            currentPosition.append("[经度]").append(location.getLongitude()+"").append("\n");
+            currentPosition.append("[维度]").append(location.getLatitude()+"").append("\n");
+            currentPosition.append("[国家]").append(location.getCountry()).append("\n");
+            currentPosition.append("[省份]").append(location.getProvince()).append("\n");
+            currentPosition.append("[城市]").append(location.getCity()).append("\n");
+            currentPosition.append("[区县]").append(location.getDistrict()).append("\n");
+            currentPosition.append("[街道]").append(location.getStreet());
+            Log.d("test",currentPosition+"");
         }
+    }
+    protected void onRefresh(){
+        finish();
+        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    private void initLocation() {
+        option.setCoorType("bd09ll");        //可选，默认gcj02，设置返回的定位结果坐标系
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        option.setOpenGps(true);        //可选，默认false,设置是否使用gps
+        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        mLocationClient.setLocOption(option);
+    }
+    private void  requestLocation(){
+        initLocation();
+        Log.d("map requestLocation:","Run requestLocation ...");
+        mLocationClient.start();
+    }
+    @Override
+    public  void  onRequestPermissionsResult(int requestCode, String[] permissions,int[] grantResults){
+        switch (requestCode){
+            case 1:
+                if(grantResults.length >0 ){
+                    for (int result : grantResults){
+                        if(result != PackageManager.PERMISSION_GRANTED){
+                            Toast.makeText(this,"all  permission  !!!",Toast.LENGTH_SHORT).show();
+                            finish();
+                            return;
+                        }
+                    }
+                    requestLocation();
+                }else {
+                    Toast.makeText(this,"Unkonw error.",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+        }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 1) {
-            flag = grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED;
-        }
+    protected void onPause() {
+        super.onPause();
     }
-
-    //通过GPS获取定位信息
-    public void getGPSLocation() {
-        Location gps = LocationUtils.getGPSLocation(this);
-        if (gps == null) {
-            //设置定位监听，因为GPS定位，第一次进来可能获取不到，通过设置监听，可以在有效的时间范围内获取定位信息
-            LocationUtils.addLocationListener(this, LocationManager.GPS_PROVIDER, new LocationUtils.ILocationListener() {
-                @Override
-                public void onSuccessLocation(Location location) {
-                    if (location != null) {
-                        Toast.makeText(MainActivity.this, "gps onSuccessLocation location:  lat==" + location.getLatitude() + "     lng==" + location.getLongitude(), Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(MainActivity.this, "gps location is null", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
-        } else {
-            Toast.makeText(this, "gps location: lat==" + gps.getLatitude() + "  lng==" + gps.getLongitude(), Toast.LENGTH_SHORT).show();
-        }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mLocationClient.stop();
     }
-
-    //通过网络等获取定位信息
-    private void getNetworkLocation() {
-        Location net = LocationUtils.getNetWorkLocation(this);
-        if (net == null) {
-            //设置定位监听
-            LocationUtils.addLocationListener(this, LocationManager.GPS_PROVIDER, new LocationUtils.ILocationListener() {
-                @Override
-                public void onSuccessLocation(Location location) {
-                    if (location != null) {
-                        Toast.makeText(MainActivity.this, "net onSuccessLocation location:  lat==" + location.getLatitude() + "     lng==" + location.getLongitude(), Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(MainActivity.this, "net location is null", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
-        } else {
-            Toast.makeText(this, "network location: lat==" + net.getLatitude() + "  lng==" + net.getLongitude(), Toast.LENGTH_SHORT).show();
+    private void JudgePermission(){
+        List<String> permissionList = new ArrayList<>();// Permission array list , request permissions in one array.
+        if(ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) !=  PackageManager.PERMISSION_GRANTED){
+            permissionList.add(android.Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if(ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED){
+            permissionList.add(android.Manifest.permission.READ_PHONE_STATE);
+        }
+        if(ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            permissionList.add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if(!permissionList.isEmpty()){
+            String[] permissions = permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(MainActivity.this,permissions,1);//request all permissions
+        }else {
+            requestLocation();// Request Location information
         }
     }
 }
